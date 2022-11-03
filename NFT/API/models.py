a05eed2fc09ed71e847cdba132fad5fb5e0a6511
Andrey582 <gandrey0606@mail.ru>
2022-11-03 23:57:43 +0300

    complete person crud

--- NFT/API/models.py
+++ NFT/API/models.py
@@ -10,16 +10,10 @@ class Person(models.Model):
 class NFTs(models.Model):
     NFTHash = models.CharField(max_length=46, verbose_name="Хэш код HFT")
 
-    def __str__(self):
-        return self.NFTHash
-
 
 class History(models.Model):
     UserAddressFrom = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name="UserFrom", verbose_name="Продавец")
     UserAddressTo = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name="UserTo", verbose_name="Покупатель")
     TransferNFTHash = models.ForeignKey(NFTs, on_delete=models.DO_NOTHING, verbose_name="Продаваемая API")
     Cost = models.FloatField(verbose_name="Цена")
-
-    def __str__(self):
-        return f"From: {self.UserAddressFrom} --- To: {self.UserAddressTo}"
 
