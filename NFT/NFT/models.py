227cca3fac6576a6b0a5717a87eac042740376ee
Andrey582 <gandrey0606@mail.ru>
2022-11-03 18:19:06 +0300

    create models

--- NFT/NFT/models.py
+++ NFT/NFT/models.py
@@ -1,1 +1,17 @@
-from
+from django.db import models
+
+
+class Person():
+    UserAddress = models.CharField(max_length=42, primary_key=True)
+    Login = models.TextField()
+    Password = models.TextField()
+
+
+class NFTs():
+    NFTHash = models.CharField(max_length=46)
+
+
+class History():
+    UserAddressFrom = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    UserAddressTo = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    TransferNFTHash = models.ForeignKey(NFTs, on_delete=models.DO_NOTHING)
